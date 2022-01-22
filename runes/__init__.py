7e3d7648db844ce2c78cc3e9e4f872f827252251
William Casarin <jb55@jb55.com>
2022-01-22 11:49:30 -0800

    init: bump version to 0.4.0
    Some package managers get confused when trying to resolve versions
    otherwise. I am hitting an error when trying to package the commando
    clightning plugin.

--- runes/__init__.py
+++ runes/__init__.py
@@ -1,6 +1,6 @@
 from .runes import Alternative, Restriction, Rune, MasterRune, check_with_reason, check, end_shastream
 
-__version__ = "0.3.1.1"
+__version__ = "0.4"
 
 __all__ = ['Alternative',
            'Restriction',
